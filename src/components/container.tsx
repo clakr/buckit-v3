79138f52c03f8a6ccdae703917af655f9266da51
clakr <clarktolosa@gmail.com>
2025-10-29 13:59:00 +0800

    feat(container): update container styles for improved layout and add new CSS variables

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -5,6 +5,13 @@ export function Container({
 	className,
 }: React.ComponentProps<"div">) {
 	return (
-		<div className={cn("p-6 flex flex-col gap-y-6", className)}>{children}</div>
+		<div
+			className={cn(
+				"p-6 flex flex-col gap-y-6 max-w-5xl mx-auto w-svw",
+				className,
+			)}
+		>
+			{children}
+		</div>
 	);
 }
