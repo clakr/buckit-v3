93152395186cabe5966f23da32e6f2afa4506afa
clakr <clarktolosa@gmail.com>
2026-07-06 12:06:40 +0300

    feat(login): build login page UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,69 @@
-import { Button } from "#/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
+import loginImage from "#/assets/login.webp";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "#/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "#/components/ui/button";
+import { IconBrandGoogleFilled } from "@tabler/icons-react";
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute("/")({ component: RouteComponent });
 
-function App() {
+function RouteComponent() {
   return (
-    <main>
-      <Button>hello world!</Button>
+    <main className="grid h-svh grid-cols-2">
+      <section className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h1 className="text-2xl font-bold">Login to your account</h1>
+          <p className="text-muted-foreground text-sm text-balance">
+            Enter your email below to login to your account
+          </p>
+        </div>
+        <form className="w-full max-w-125 p-6">
+          <FieldGroup className="gap-y-6">
+            <Field>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
+              <Input id="email" type="email" placeholder="youremail@example.com" required />
+            </Field>
+            <Field>
+              <div className="flex items-center justify-between">
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <a href="#" className="text-sm">
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" type="password" placeholder="********" required />
+            </Field>
+            <Field>
+              <Button type="submit">Login</Button>
+            </Field>
+            <FieldSeparator>Or continue with</FieldSeparator>
+            <Field className="gap-y-3">
+              <Button variant="outline" type="button">
+                <IconBrandGoogleFilled />
+                Login with Google
+              </Button>
+              <FieldDescription className="text-center">
+                Don&apos;t have an account?{" "}
+                <a href="#" className="underline underline-offset-4">
+                  Sign up
+                </a>
+              </FieldDescription>
+            </Field>
+          </FieldGroup>
+        </form>
+      </section>
+      <section className="relative isolate">
+        <img
+          src={loginImage}
+          alt=""
+          className="absolute inset-2 size-[calc(100%-(--spacing(4)))] rounded-md"
+        />
+      </section>
     </main>
   );
 }
